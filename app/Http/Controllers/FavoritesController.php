22e03fb30b884a032c401e9dceba59a001347a58
Nicholas Hoium <nhoium@gmail.com>
2018-01-12 13:24:07 -0600

    Reputation is gained/reduced from a reply being favorited/unfavorited

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Reply;
+use App\Reputation;
 
 class FavoritesController extends Controller
 {
@@ -15,11 +16,13 @@ class FavoritesController extends Controller
     {
         $reply->favorite();
 
-        return redirect()->back();
+        Reputation::award($reply->owner, Reputation::REPLY_FAVORITED);
     }
 
     public function destroy(Reply $reply)
     {
         $reply->unfavorite();
+
+        Reputation::reduce($reply->owner, Reputation::REPLY_FAVORITED);
     }
 }
