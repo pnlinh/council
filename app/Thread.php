790cefe4ba2080f73d6443a6c5689ed18b0b3447
Nicholas Hoium <nhoium@gmail.com>
2018-01-12 13:23:38 -0600

    Reputation is reduced when a thread is deleted

--- app/Thread.php
+++ app/Thread.php
@@ -24,6 +24,8 @@ class Thread extends Model
 
         static::deleting(function ($thread) {
             $thread->replies->each->delete();
+
+            Reputation::reduce($thread->creator, Reputation::THREAD_WAS_PUBLISHED);
         });
 
         static::created(function ($thread) {
